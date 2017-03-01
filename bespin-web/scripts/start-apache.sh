400e9442e2202ae54f977fb1b69e298b55ee9341
Dan Leehr <dan.leehr@duke.edu>
2017-03-01 16:23:46 -0500

    Moves create-lando-user and load-sample-data into the start-apache script
    This logs an error if the lando user already exists, but it makes sense to do on startup, so we should update the createusertoken command to indicate that the user exists

--- bespin-web/scripts/start-apache.sh
+++ bespin-web/scripts/start-apache.sh
@@ -3,11 +3,7 @@
 # ensure database is migrated before each start of the production application
 python manage.py migrate
 
-# Check if any data needs to be loaded
-# if [ -f /etc/external/fixtures.json ]; then
-#   /usr/local/bin/python manage.py loaddata /etc/external/fixtures.json
-# fi
-
-# Update the variable names
+create-lando-user.sh
+load-sample-data.sh
 
 apachectl -DFOREGROUND
