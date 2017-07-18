777c186ae1a6cfd4ac5a8d724cbf569c24d87aa8
John Bradley <johnbradley2008@gmail.com>
2017-07-18 10:50:48 -0400

    fix command name

--- bespin-web/scripts/create-group-manager-connection.sh
+++ bespin-web/scripts/create-group-manager-connection.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python manage.py creategmsettings ${GROUP_MANAGER_ACCOUNT_ID} ${GROUP_MANAGER_PASSWORD}
+python manage.py creategroupmanagerconnection ${GROUP_MANAGER_ACCOUNT_ID} ${GROUP_MANAGER_PASSWORD}
